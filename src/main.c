0eff40d815283cef1ce7e75ca090716cd693fa8b
Matt Ashford <matt.ashford@hotmail.co.nz>
2024-04-16 00:04:10 +1000

    progress

--- src/main.c
+++ src/main.c
@@ -2,12 +2,13 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-SDL_Window *window;
-SDL_Renderer *renderer;
+bool is_running = true;
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
 
 bool initialize_window(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-    fprintf(stderrm "Error initializing SDL. \n");
+    fprintf(stderr, "Error initializing SDL. \n");
     return false;
   }
 
@@ -15,19 +16,52 @@ bool initialize_window(void) {
       SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                        800, 600, SDL_WINDOW_BORDERLESS);
   if (!window) {
-    fprintf(stderr, "Error creating SDL window. \m");
+    fprintf(stderr, "Error creating SDL window. \n");
     return false;
+  }
+  renderer = SDL_CreateRenderer(window, -1, 0);
+  if (!renderer) {
+    fprintf(stderr, "Error creating renderer. \n");
+    return false;
+  }
+  return true;
+}
 
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    if (!renderer) {
-      fprintf(stderr, "Error creating renderer");
-    }
-    return true;
+void setup(void) {}
+
+void process_input(void) {
+  SDL_Event event;
+  SDL_PollEvent(&event);
+
+  switch (event.type) {
+  case SDL_QUIT:
+    is_running = false;
+    break;
+  case SDL_KEYDOWN:
+    if (event.key.keysym.sym == SDLK_ESCAPE)
+      is_running = false;
+    break;
+  }
+}
+void update(void) {}
+
+void render(void) {
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  SDL_RenderClear(renderer);
+
+  SDL_RenderPresent(renderer);
+}
+
+int main(void) {
+  is_running = initialize_window();
+
+  setup();
+
+  while (is_running) {
+    process_input();
+    update();
+    render();
   }
 
-  int main(void) {
-
-    initialize_window();
-
-    return 0;
-  }
+  return 0;
+}
