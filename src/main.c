51302530d72e960af58467fbeb792ef7ca148ac2
Matt Ashford <matt.ashford@hotmail.co.nz>
2024-05-21 23:48:31 +1000

    changing structure

--- src/main.c
+++ src/main.c
@@ -5,13 +5,6 @@
 #include <stdint.h>
 #include <stdio.h>
 
-///////////////////////////////////////////////////////////////////////////////
-/// Declare an array of vectors/points
-///////////////////////////////////////////////////////////////////////////////
-const int N_POINTS = 9 * 9 * 9;
-vec3_t cube_points[N_POINTS]; // 9x9x9 cube
-vec2_t projected_points[N_POINTS];
-
 vec3_t camera_position = {.x = 0, .y = 0, .z = -5};
 vec3_t cube_rotation = {.x = 0, .y = 0, .z = 0};
 
@@ -30,19 +23,6 @@ void setup(void) {
   color_buffer_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
                                            SDL_TEXTUREACCESS_STREAMING,
                                            window_width, window_height);
-
-  int point_count = 0;
-
-  // Start loading my array of vectors
-  // from -1 to 1 (in this 9x9x9 cube)
-  for (float x = -1; x <= 1; x += 0.25) {
-    for (float y = -1; y <= 1; y += 0.25) {
-      for (float z = -1; z <= 1; z += 0.25) {
-        vec3_t new_point = {.x = x, .y = y, .z = z};
-        cube_points[point_count++] = new_point;
-      }
-    }
-  }
 }
 
 void process_input(void) {
@@ -87,34 +67,34 @@ void update(void) {
   cube_rotation.y += 0.01;
   cube_rotation.z += 0.01;
 
-  for (int i = 0; i < N_POINTS; i++) {
-    vec3_t point = cube_points[i];
-
-    vec3_t transformed_point = vec3_rotate_x(point, cube_rotation.x);
-    transformed_point = vec3_rotate_y(transformed_point, cube_rotation.y);
-    transformed_point = vec3_rotate_z(transformed_point, cube_rotation.z);
-
-    // Translate the points away from the camera
-    transformed_point.z -= camera_position.z;
-
-    // Project the current point
-    vec2_t projected_point = project(transformed_point);
-
-    // Save the projected 2DR vector in the array of projected points
-    projected_points[i] = projected_point;
-  }
+  // for (int i = 0; i < N_POINTS; i++) {
+  //   vec3_t point = cube_points[i];
+  //
+  //   vec3_t transformed_point = vec3_rotate_x(point, cube_rotation.x);
+  //   transformed_point = vec3_rotate_y(transformed_point, cube_rotation.y);
+  //   transformed_point = vec3_rotate_z(transformed_point, cube_rotation.z);
+  //
+  //   // Translate the points away from the camera
+  //   transformed_point.z -= camera_position.z;
+  //
+  //   // Project the current point
+  //   vec2_t projected_point = project(transformed_point);
+  //
+  //   // Save the projected 2DR vector in the array of projected points
+  //   projected_points[i] = projected_point;
+  // }
 }
 
 void render(void) {
   draw_grid();
-
-  // Loop all projected points and render them
-  for (int i = 0; i < N_POINTS; i++) {
-    vec2_t projected_point = projected_points[i];
-    draw_rect(projected_point.x + (window_width / 2),
-              projected_point.y + (window_height / 2), 4, 4, 0xFFFFFF00);
-  }
-
+  //
+  // // Loop all projected points and render them
+  // for (int i = 0; i < N_POINTS; i++) {
+  //   vec2_t projected_point = projected_points[i];
+  //   draw_rect(projected_point.x + (window_width / 2),
+  //             projected_point.y + (window_height / 2), 4, 4, 0xFFFFFF00);
+  // }
+  //
   render_color_buffer();
   clear_color_buffer(0x00000000);
 
