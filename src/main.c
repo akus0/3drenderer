fd3b5965dab148e9b85481a3c9a4e31d34aa5998
Matt Ashford <matt.ashford@hotmail.co.nz>
2024-05-21 23:22:23 +1000

    progress

--- src/main.c
+++ src/main.c
@@ -18,6 +18,7 @@ vec3_t cube_rotation = {.x = 0, .y = 0, .z = 0};
 float fov_factor = 640;
 
 bool is_running = false;
+int previous_frame_time = 0;
 
 void setup(void) {
   // Allocate the required memory in bytes to hold the color buffer
@@ -71,9 +72,20 @@ vec2_t project(vec3_t point) {
 }
 
 void update(void) {
-  cube_rotation.x += 0.001;
-  cube_rotation.y += 0.001;
-  cube_rotation.z += 0.001;
+
+  // Wait some time until reach the tatget frame time in milliseconds
+  int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks() - previous_frame_time);
+
+  // Only delay execution if we are running too fast
+  if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME) {
+    SDL_Delay(time_to_wait);
+  }
+
+  previous_frame_time = SDL_GetTicks();
+
+  cube_rotation.x += 0.01;
+  cube_rotation.y += 0.01;
+  cube_rotation.z += 0.01;
 
   for (int i = 0; i < N_POINTS; i++) {
     vec3_t point = cube_points[i];
