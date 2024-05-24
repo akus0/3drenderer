e7ebe941bf0e88fb36a91da9e2af1f8e723fff7d
Matt Ashford <matt.ashford@hotmail.co.nz>
2024-05-24 21:17:36 +1000

    drawing cube with lines

--- src/display.h
+++ src/display.h
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define FPS 30
+#define FPS 60
 #define FRAME_TARGET_TIME (1000 / FPS)
 
 extern SDL_Window *window;
@@ -18,6 +18,9 @@ bool initialize_window(void);
 
 void draw_grid(void);
 void draw_pixel(int x, int y, uint32_t color);
+void draw_line(int x0, int y0, int x1, int y1, uint32_t color);
+void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2,
+                   uint32_t color);
 void draw_rect(int x, int y, int width, int height, uint32_t color);
 void render_color_buffer(void);
 void clear_color_buffer(uint32_t color);
--- src/main.c
+++ src/main.c
@@ -126,8 +126,15 @@ void render(void) {
     draw_rect(triangle.points[0].x, triangle.points[0].y, 3, 3, 0xFFFFFF00);
     draw_rect(triangle.points[1].x, triangle.points[1].y, 3, 3, 0xFFFFFF00);
     draw_rect(triangle.points[2].x, triangle.points[2].y, 3, 3, 0xFFFFFF00);
+
+    draw_triangle(triangle.points[0].x, triangle.points[0].y,
+                  triangle.points[1].x, triangle.points[1].y,
+                  triangle.points[2].x, triangle.points[2].y, 0xFFFFFF00);
   }
 
+  // draw_line(100, 200, 300, 50, 0xFF00FF00);
+  // draw_line(300, 50, 400, 600, 0xFF00FF00);
+  // draw_line(800, 800, 100, 700, 0xFF00FF00);
   render_color_buffer();
   clear_color_buffer(0x00000000);
 
